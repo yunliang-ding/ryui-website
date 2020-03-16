2efee6953192dd5fb9cbd2f03a7b83f2b451b9ee
yunliang <yunliang@ding.com>
2020-03-16 20:59:38 +0800

    Button opt

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -6,64 +6,112 @@ export default class extends React.Component {
     return (
       <div className="app-button">
         <div className="app-button-left">
-          <Button style={{ width: 100, margin: 4 }} label="normal" onClick={() => {
-            alert('normal')
-          }} />
-          <Button type="primary" style={{ width: 100, margin: 4 }} label="primary" onClick={() => {
-            alert('primary')
-          }} />
-          <Button type="warm" style={{ width: 100, margin: 4 }} label="warm" onClick={() => {
+          <div style={{display: 'flex'}}>
+            <Button style={{ width: 100, margin: 4 }} label="normal" onClick={() => {
+              alert('normal')
+            }} />
+            <Button type="primary" style={{ width: 100, margin: 4 }} label="primary" onClick={() => {
+              alert('primary')
+            }} />
+            <Button type="warm" style={{ width: 100, margin: 4 }} label="warm" onClick={() => {
 
-          }} />
-          <Button type="blue" style={{ width: 100, margin: 4 }} label="blue" onClick={() => {
+            }} />
+            <Button type="blue" style={{ width: 100, margin: 4 }} label="blue" onClick={() => {
 
-          }} />
-          <Button type="danger" style={{ width: 100, margin: 4 }} label="danger" onClick={() => {
+            }} />
+            <Button type="danger" style={{ width: 100, margin: 4 }} label="danger" onClick={() => {
 
-          }} />
-          <Button type="disabled" style={{ width: 100, margin: 4 }} label="disabled" onClick={() => {
+            }} />
+            <Button type="disabled" style={{ width: 100, margin: 4 }} label="disabled" onClick={() => {
 
-          }} />
-          <Button style={{ width: 100, margin: 4 }} label={
-            <i className="iconfont icon-chazhao" />
-          } onClick={() => {
-            alert('iconfont')
-          }} />
-          <Button type="primary" style={{ width: 100, margin: 4 }} label={
-            <i className="iconfont icon-chazhao" />
-          } onClick={() => {
-            alert('icon-chuangjian')
-          }} />
-          <Button type="warm" style={{ width: 100, margin: 4 }} label={
-            <i className="iconfont icon-chazhao" />
-          } onClick={() => {
+            }} />
+          </div>
+          <div style={{display: 'flex'}}>
+            <Button style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-fenxiang" />
+            } onClick={() => {
+              alert('iconfont')
+            }} />
+            <Button type="primary" style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-git" />
+            } onClick={() => {
+              alert('icon-chuangjian')
+            }} />
+            <Button type="warm" style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-icon_yingyongguanli" />
+            } onClick={() => {
 
-          }} />
-          <Button type="blue" style={{ width: 100, margin: 4 }} label={
-            <i className="iconfont icon-chazhao" />
-          } onClick={() => {
+            }} />
+            <Button type="blue" style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-tuichu" />
+            } onClick={() => {
 
-          }} />
-          <Button type="danger" style={{ width: 100, margin: 4 }} label={
-            <i className="iconfont icon-chazhao" />
-          } onClick={() => {
+            }} />
+            <Button type="danger" style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-github" />
+            } onClick={() => {
 
-          }} />
-          <Button type="disabled" style={{ width: 100, margin: 4 }} label={
-            <i className="iconfont icon-chazhao" />
-          } onClick={() => {
+            }} />
+            <Button type="disabled" style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-chazhao" />
+            } onClick={() => {
 
-          }} />
+            }} />
+          </div>
         </div>
         <div className="app-button-right">
-          <Button dark style={{ width: 100, margin: 4 }} label="normal" onClick={() => {
-            alert('normal')
-          }} />
-          <Button dark style={{ width: 100, margin: 4 }} label={
-            <i className="iconfont icon-chazhao" />
-          } onClick={() => {
-            alert('iconfont')
-          }} />
+          <div style={{display: 'flex'}}>
+            <Button dark style={{ width: 100, margin: 4 }} label="normal" onClick={() => {
+              alert('normal')
+            }} />
+            <Button type="primary" style={{ width: 100, margin: 4 }} label="primary" onClick={() => {
+              alert('primary')
+            }} />
+            <Button type="warm" style={{ width: 100, margin: 4 }} label="warm" onClick={() => {
+
+            }} />
+            <Button type="blue" style={{ width: 100, margin: 4 }} label="blue" onClick={() => {
+
+            }} />
+            <Button type="danger" style={{ width: 100, margin: 4 }} label="danger" onClick={() => {
+
+            }} />
+            <Button dark type="disabled" style={{ width: 100, margin: 4 }} label="disabled" onClick={() => {
+
+            }} />
+          </div>
+          <div style={{display: 'flex'}}>
+            <Button dark style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-fenxiang" />
+            } onClick={() => {
+              alert('iconfont')
+            }} />
+            <Button type="primary" style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-git" />
+            } onClick={() => {
+              alert('icon-chuangjian')
+            }} />
+            <Button type="warm" style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-icon_yingyongguanli" />
+            } onClick={() => {
+
+            }} />
+            <Button type="blue" style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-tuichu" />
+            } onClick={() => {
+
+            }} />
+            <Button type="danger" style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-github" />
+            } onClick={() => {
+
+            }} />
+            <Button dark type="disabled" style={{ width: 100, margin: 4 }} label={
+              <i className="iconfont icon-chazhao" />
+            } onClick={() => {
+
+            }} />
+          </div>
         </div>
       </div>
     )
