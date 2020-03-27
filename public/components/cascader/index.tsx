09ef7769ece2da33d1e3f36572fa3bbcd18467bf
yunliang <yunliang@ding.com>
2020-03-27 16:21:01 +0800

    cascader

--- public/components/cascader/index.tsx
+++ public/components/cascader/index.tsx
@@ -2,11 +2,94 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { Cascader } from 'Yui'
 class Demo extends React.Component {
+  dataList = [
+    {
+      value: '1',
+      label: 'label 1',
+      children: [
+        {
+          value: '1-1',
+          label: 'label 1-1',
+          children: [
+            {
+              value: '1-1-1',
+              label: 'label 1-1-1'
+            },
+            {
+              value: '1-1-2',
+              label: 'label 1-1-2'
+            }
+          ]
+        },
+        {
+          value: '1-2',
+          label: 'label 1-2',
+          children: [
+            {
+              value: '1-2-1',
+              label: 'label 1-2-1'
+            },
+            {
+              value: '1-2-2',
+              label: 'label 1-2-2'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      value: '2',
+      label: 'label 2',
+      children: [
+        {
+          value: '2-1',
+          label: 'label 2-1',
+          children: [
+            {
+              value: '2-1-1',
+              label: 'label 2-1-1'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+  state = {
+    value: ['1', '1-2', '1-2-2']
+  }
   render() {
     const dark = true
     return (
       <div className="app-preview">
-        <Cascader dark={dark} style={{width: 200}} />
+        <Cascader
+          dark={dark}
+          placeholder='选择城市'
+          style={{ width: 200 }}
+          dataList={this.dataList}
+        />
+        <br /><br />
+        <Cascader
+          dark={dark}
+          placeholder=''
+          style={{ width: 200 }}
+          dataList={this.dataList}
+          value={this.state.value}
+          onChange={
+            (value) => {
+              this.setState({
+                value
+              })
+            }
+          }
+        />
+        <br /> <br />
+        <Cascader
+          dark={dark}
+          trigger='hover'
+          placeholder='trigger-hover'
+          style={{ width: 200 }}
+          dataList={this.dataList}
+        />
       </div>
     )
   }
--- src/Yui/cascader/index.tsx
+++ src/Yui/cascader/index.tsx
@@ -2,14 +2,98 @@ import * as React from "react"
 import './index.less'
 class Cascader extends React.Component {
   props: any;
-  state = {
-    isOpen: false
+  state: any;
+  cascaderNode: any;
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: false,
+      cols: [JSON.parse(JSON.stringify(props.dataList))],
+      label: ''
+    }
+  }
+  componentDidMount() {
+    const { value } = this.props
+    if (Array.isArray(value)) {
+      value.map((v, index) => { // 依次选中并展开
+        this.setColsByValue(this.state.cols[index], v)
+      })
+      this.setState({
+        label: this.getLabel(value),
+        cols: this.state.cols
+      })
+    }
+  }
+  componentWillReceiveProps(props) {
+    if (props.value) {
+      this.setState({
+        label: this.getLabel(props.value)
+      })
+    }
+  }
+  setColsByValue = (col, value) => {// 选中并展开
+    col.map(_col => {
+      if (_col.value === value) {
+        _col.selected = true
+        _col.children && this.state.cols.push(_col.children)
+      }
+    })
+  }
+  getLabel = (value) => {
+    if (!value) { return '' }
+    const labels = this.state.cols.map((col, index) => {
+      const selected = col.find(_col => {
+        if (_col.value === value[index]) {
+          _col.selected = true
+          return true
+        }
+      }) || {}
+      return selected.label
+    })
+    return labels.join('/')
+  }
+  complete = () => {
+    // 找出所有选中的value返回
+    const value = this.state.cols.map(col => {
+      return col.find(_col => _col.selected).value
+    })
+    const label = this.getLabel(value)
+    this.setState({
+      isOpen: false,
+      label
+    }, () => {
+      if (this.props.onChange) {
+        this.props.onChange(value)
+      }
+    })
+  }
+  labelClick = (cols, col, item, index) => {
+    col.map(_col => { _col.selected = _col.value === item.value }) // 这个选中
+    if (item.children) {
+      // 后面全部清空, 重制不选中
+      cols.splice(index + 1, cols.length).forEach(col => col.map(_col => _col.selected = false))
+      cols.push(JSON.parse(JSON.stringify(item.children))) // 添加新的
+      this.setState({ // render
+        cols: this.state.cols
+      })
+    } else { // 结束选择
+      this.setState({
+        cols: this.state.cols
+      }, () => {
+        this.complete()
+      })
+    }
+  }
+  componentDidUpdate() {
+    const { isOpen } = this.state
+    this.cascaderNode.style.borderColor = isOpen ? 'var(--theme-color)' : (this.props.dark ? '#262626' : '#ccc')
   }
   render() {
-    const { style, dark } = this.props
+    const { style, dark, placeholder, trigger } = this.props
     let theme = dark ? '-dark' : ''
-    const { isOpen } = this.state
+    const { isOpen, cols, label } = this.state
     let cascader = <div
+      ref={(node) => { this.cascaderNode = node }}
       style={style}
       className={'yui-cascader' + theme}
       onClick={
@@ -24,6 +108,9 @@ class Cascader extends React.Component {
         <i className={isOpen ? 'iconfont icon-jiantou' : 'iconfont icon-jiantou32'}></i>
       </div>
       {
+        label ? <div className='yui-cascader-label'>{label}</div> : <div className='yui-cascader-placeholder'>{placeholder || '请选择'}</div>
+      }
+      {
         this.state.isOpen && [
           <div className='yui-cascader-body-spin' />,
           <div className='yui-cascader-body-allow' />,
@@ -32,31 +119,34 @@ class Cascader extends React.Component {
               e.stopPropagation()
             }
           }>
-            <div className='yui-cascader-body-content'>
-              <div className='yui-cascader-body-content-item'>
-                <span>zhejiang</span>
-                <i className='iconfont icon-jiantou1'></i>
-              </div>
-              <div className='yui-cascader-body-content-item'>
-                <span>anhui</span>
-                <i className='iconfont icon-jiantou1'></i>
-              </div>
-            </div>
-            <div className='yui-cascader-body-content'>
-              {
-                <div className='yui-cascader-body-content-item'>
-                  <span>hangzhou</span>
-                  <i className='iconfont icon-jiantou1'></i>
+            {
+              cols.map((col, index) => {
+                return <div className='yui-cascader-body-col'>
+                  {
+                    col.map(item => {
+                      return <div
+                        className={item.selected ? 'yui-cascader-body-col-item-active' : 'yui-cascader-body-col-item'}
+                        onMouseOver={
+                          () => {
+                            if(trigger === 'hover' && item.children){
+                              this.labelClick(cols, col, item, index)
+                            }
+                          }
+                        }
+                        onClick={
+                          () => {
+                            this.labelClick(cols, col, item, index)
+                          }
+                        }
+                      >
+                        <span>{item.label}</span>
+                        {item.children && <i className='iconfont icon-jiantou1'></i>}
+                      </div>
+                    })
+                  }
                 </div>
-              }
-            </div>
-            <div className='yui-cascader-body-content'>
-              {
-                <div className='yui-cascader-body-content-item'>
-                  <span>xihuqu</span>
-                </div>
-              }
-            </div>
+              })
+            }
           </div>
         ]
       }
