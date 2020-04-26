768ccc1a9b30ab8866488c25608f0df26003ad04
yunliang <yunliang@ding.com>
2020-04-26 17:03:28 +0800

    Carousel

--- public/components/carousel/index.tsx
+++ public/components/carousel/index.tsx
@@ -4,9 +4,14 @@ import ReactDom from 'react-dom'
 import { Carousel } from 'ryui'
 class Demo extends React.Component {
   render() {
+    const pages = [<div>page1</div>, <div>page2</div>, <div>page3</div>, <div>page4</div>,]
     return (
       <div className="app-preview">
-        <Carousel dataList={[]} />
+        <Carousel
+          style={{ width: 500, height: 400 }}
+          pages={pages}
+          currentPage={2}
+        />
       </div>
     )
   }
--- src/yui/carousel/index.tsx
+++ src/yui/carousel/index.tsx
@@ -2,24 +2,78 @@ import * as React from "react"
 import './index.less'
 const Window: any = window
 class Carousel extends React.Component {
+  state: any
   props: {
-    dark?:boolean,
-    style?:any,
-    dataList:any
+    dark?: boolean,
+    style?: any,
+    pages: any,
+    currentPage?: any,
+    onChange?: any
   }
   constructor(props) {
     super(props)
+    this.state = {
+      currentPage: props.currentPage
+    }
+  }
+  componentWillReceiveProps(props) {
+    this.state.currentPage = props.currentPage
   }
   render() {
-    const { style, dataList } = this.props
+    const { style, pages } = this.props
     const dark = this.props.dark || Window.yuiIsDark
+    const { currentPage } = this.state
     let theme = dark ? '-dark' : ''
     return <div className={`yui-carousel${theme}`} style={style}>
+      <div className='yui-carousel-before' onClick={
+        () => {
+          this.setState({
+            currentPage: this.state.currentPage == 1 ? 1 : this.state.currentPage - 1
+          })
+        }
+      }>
+        <i className='iconfont icon-icon-jiantouzuo'></i>
+      </div>
+      <div className='yui-carousel-next' onClick={
+        () => {
+          this.setState({
+            currentPage: this.state.currentPage === pages.length ? pages.length : this.state.currentPage + 1
+          })
+        }
+      }>
+        <i className='iconfont icon-jiantou2'></i>
+      </div>
       {
-        dataList.map(item => {
-          return <div>yui</div>
+        pages && pages.map((page, index) => {
+          return <div className='yui-carousel-page' style={{
+            left: (index + 1) === currentPage ? 0 : 100 * ((index+1) - currentPage) + '%'
+          }}>
+            {
+              page
+            }
+          </div>
         })
       }
+      <div className='yui-carousel-legend'>
+        <div className='yui-carousel-legend-box'>
+          {
+            pages && pages.map((page, index) => {
+              return <div
+                className={(index + 1) === currentPage ? 'yui-carousel-legend-box-item-active' : 'yui-carousel-legend-box-item'}
+                onClick={
+                  () => {
+                    this.setState({
+                      currentPage: index + 1
+                    }, () => {
+                      this.props.onChange && this.props.onChange(index + 1)
+                    })
+                  }
+                }
+              />
+            })
+          }
+        </div>
+      </div>
     </div>
   }
 }
